3561b4ebb27b4d0b3d62eb417dd1b36da00b22bc
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2018-09-27 19:30:55 -0700

    set things in layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,42 @@
 <template>
-  <div>
+  <v-app>
+    <v-navigation-drawer v-model="sideNav" fixed app>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary" app>
+      <v-toolbar-side-icon class="hidden-sm-and-up" @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left dark>home</v-icon>
+          Home
+        </v-btn>
+        <v-btn flat>
+          <v-icon left dark>contact_mail</v-icon>
+          Contact
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <nuxt/>
-  </div>
+  </v-app>
 </template>
 
 <style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,44 +1,7 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer v-model="sideNav" absolute app>
-      <v-list>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Contact</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar dark class="primary" app>
-      <v-toolbar-side-icon class="hidden-sm-and-up" @click.stop="sideNav = !sideNav"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left dark>home</v-icon>
-          Home
-        </v-btn>
-        <v-btn flat>
-          <v-icon left dark>contact_mail</v-icon>
-          Contact
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-    <main>
-      test
-    </main>
-  </v-app>
+  <div>
+    test2
+  </div>
 </template>
 
 <script>
