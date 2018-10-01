24775cd25ba46f301230604b5a81031020a39761
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2018-09-30 19:22:49 -0700

    project mockup

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -2,11 +2,17 @@
   <div class="text-xs-center">
     <section-default :styleHeight="styleHeight">
       <template>
-        <v-flex sm10>
+        <v-flex sm12 md6>
+          <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="1.3"/>
+        </v-flex>
+        <v-flex sm12 md6>
           <h1>Project 1</h1>
-          <pre>
-NOTE: Need contents
-          </pre>
+          <p>
+            Fashion axe pop-up paleo, pok pok celiac try-hard biodiesel shaman portland mumblecore next level art party hella. Microdosing green juice blue bottle, la croix knausgaard copper mug cred helvetica man bun lo-fi brunch. Retro tacos ugh celiac man braid. Flexitarian jean shorts hoodie, slow-carb cold-pressed drinking vinegar selfies put a bird on it raclette la croix. Edison bulb next level truffaut tacos freegan, etsy jianbing retro pabst 8-bit gochujang woke intelligentsia.
+          </p>
+          <p>
+            Kickstarter kitsch next level +1 DIY brunch. Whatever pug irony tofu mumblecore woke, venmo pop-up put a bird on it. Waistcoat ennui banh mi squid. La croix hashtag glossier messenger bag activated charcoal, 8-bit fashion axe.
+          </p>
         </v-flex>
       </template>
     </section-default>
