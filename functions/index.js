518fa7ebd67a5443aa3801e7b7a37ac82cc098e4
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2018-10-23 21:53:30 -0700

    just replaced tab with spaces

--- functions/index.js
+++ functions/index.js
@@ -5,21 +5,21 @@ const { Nuxt } = require('nuxt')
 const app = express()
 
 const config = {
-	dev: false,
-	buildDir: 'nuxt',
-	build: {
-		publicPath: '/'
-	}
+  dev: false,
+  buildDir: 'nuxt',
+  build: {
+    publicPath: '/'
+  }
 }
 const nuxt = new Nuxt(config)
 
 function handleRequest(req, res) {
-	res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
-	nuxt.renderRoute('/').then(result => {
-		res.send(result.html)
-	}).catch(e => {
-		res.send(e)
-	})
+  res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
+  nuxt.renderRoute('/').then(result => {
+    res.send(result.html)
+  }).catch(e => {
+    res.send(e)
+  })
 }
 app.get('*', handleRequest)
 exports.nuxtApp = functions.https.onRequest(app)
