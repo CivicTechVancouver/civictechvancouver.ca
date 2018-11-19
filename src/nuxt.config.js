c992867e9e62790b11045254f470d2d05f719c82
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2018-11-19 12:10:33 -0800

    indexing

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -5,7 +5,6 @@ module.exports = {
   head: {
     title: 'Civic Tech Vancouver',
     meta: [
-      { name: 'robots', content: 'noindex' },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Civic Tech Vancouver' }
